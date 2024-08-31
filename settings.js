c440b1f138d71290181fdc9b0ba06e0a90cbfce1
Duxlin <folorunshoabdulquodir1@gmail.com>
2024-08-31 23:24:27 +0100

    hm

---
+++ settings.js
@@ -0,0 +1,17 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['2347082252014','2349167632397']
+global.botname = 'Dux'
+global.namaowner = 'Dux'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Dux"
+global.sticker2 = "🌜"
+global.apikey = '_'
+global.capikey = '_'
+global.domain = '_'
+global.eggsnya = '_'
+global.location = '_'
+global.adana = '234x'
+global.aqris = 'https://telegra.ph/file/1701509104e6531c8a4d1.jpg'
+global.aovo = '234c'
+global.agopay = '628XX'
